9bcdd6920943ce5b4ce2bfc37ade7867edc41473
rajjangid399@gmail.com <rejjangid3999@gmail.com>
2026-03-23 16:18:12 +0530

    change backend url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://careerconnect-8ip9.onrender.com";
 
 const clientServer = axios.create({
   baseURL: BASE_URL,
